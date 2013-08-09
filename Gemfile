1af7f28efc21a74235c8d544e7ea039cd19107ed
Shane <Shane@Shanes-MacBook-Pro.local>
2013-08-09 10:32:14 -0700

    Converting to Bogus for fakes to ensure I don't fake methods that don't exist.

--- Gemfile
+++ Gemfile
@@ -3,6 +3,7 @@ source 'https://rubygems.org'
 group :development do
   gem 'guard'
   gem 'guard-minitest'
+  gem 'bogus'
 end
 
 gem 'fxruby'
--- test/test_ui.rb
+++ test/test_ui.rb
@@ -1,61 +1,74 @@
 require 'minitest'
+require 'bogus/minitest/spec'
 require_relative '../ui.rb'
 require_relative './test_ui_interface.rb'
+require_relative '../ui_text_adapter.rb'
 
 def mock_ui_adapter
   MiniTest::Mock.new
+end
+
+def fake_ui_adapter
+  fake(:UITextAdapter, write: nil)
+end
+
+def stub_read(adapter, result)
+  stub(adapter).read(any_args) { result }
+end
+
+def stub_ask(adapter, result)
+  stub(adapter).ask(any_args) { result }
 end
 
 describe UI do
   include UIInterfaceTest
   
   before do
-    @adapter = mock_ui_adapter
+    @adapter = fake_ui_adapter
     @ui = UI.new(@adapter)
+  end
+
+  it "can identify bogus calls" do
+    expected = "Yes"
+    stub_read(@adapter, expected)
+    #example of a mistake bogus would catch below
+    # stub_ask(@adapter, expected)
+    @ui.ask("Can you hear me now?").must_equal expected
+    # @adapter.must_have_received :read, question
   end
 
   it "can tell the user something" do
     statement = "something"
-    @adapter.expect :write, nil, [statement]
     @ui.tell(statement).must_be_nil
-    @adapter.verify
   end
 
   it "can ask the user for input" do
-    question = "Can you hear me now?"
-    @adapter.expect :read, 'y', [question]
-    @ui.ask(question).must_equal 'y'
-    @adapter.verify
+    expected = "Yes"
+    stub_read(@adapter, expected)
+    @ui.ask("Can you hear me now?").must_equal expected
   end
 
   it "will return true if the user answers yes" do
-    question = "Huh?"
-    @adapter.expect :read, 'Yes', [@ui.append_option_text(question)]
-    @ui.ask_yes_no(question).must_equal true
-    @adapter.verify
+    stub_read(@adapter, "Yes")
+    @ui.ask_yes_no("Do you think yes should return true?").must_equal true
   end
 
   it "will return false if the user answers no" do
-    question = "Huh?"
-    @adapter.expect :read, 'No', [@ui.append_option_text(question)]
-    @ui.ask_yes_no(question).must_equal false
-    @adapter.verify
+    stub_read(@adapter, "No")
+    @ui.ask_yes_no("Is testing a waste of time?").must_equal false
   end
 
   it "will return nil if the user answers skip" do
-    question = "Huh?"
-    @adapter.expect :read, 'Skip', [@ui.append_option_text(question)]
-    @ui.ask_yes_no(question).must_be_nil
-    @adapter.verify
+    stub_read(@adapter, "skip")
+    @ui.ask_yes_no("Are you stumped by this question?").must_be_nil
   end
 
   it "will quit if the user enters quit" do
-    text = "Enter q or quit to exit."
-    @adapter.expect :read, 'q', [text]
     ui = UI.new(@adapter)
-    def ui.quit
-      @adapter.verify.must_equal true
-    end
-    ui.ask(text)
+    #Stub this because I don't really want to call exit in the middle of running my test.
+    stub(ui).quit
+    stub_read(@adapter, "quit")
+    ui.ask("Would you like to quit?")
+    ui.must_have_received :quit, []
   end
 end
